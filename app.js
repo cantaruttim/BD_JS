1146ddc8da209ea58e94b07cf3a81075516bd9a0
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2022-08-02 12:24:20 -0300

    conection ready

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require('express');
 const app = express();
 const User = require('./models/User');
 
+app.use(express.json()); // app recebendo dados de forma correta no formato JSON
+
 app.get("/", async (req, res) => { // tipo get
     res.send("Página Inicial - CRUD com BD do Matheus");
 });
 
 app.post("/cadastrar", async (req, res) => { // rota do tipo post
-    res.send("Página Cadastrar");
+    // console.log(req.body);
+
+    await User.create(req.body)
+    .then( () => {
+        return res.json({
+            erro: false,
+            mensagem: "Usuário cadastrado com sucesso!"
+        });
+    }).catch( () => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuário não cadastrado com sucesso!"
+        });
+    });
+
 });
 
 app.listen(8080, () => {
