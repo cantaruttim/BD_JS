f375906fc24086652c56a748c2188e8036137f12
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2022-08-02 12:41:16 -0300

    new table: Projects

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const User = require('./models/User');
+const Project = require('./models/Project');
 
 app.use(express.json()); // app recebendo dados de forma correta no formato JSON
 
@@ -25,6 +26,26 @@ app.post("/cadastrar", async (req, res) => { // rota do tipo post
     });
 
 });
+
+app.post("/projects", async (req_project, res_project) => { // rota do tipo post
+    //console.log(req_project.body);
+
+    await Project.create(req_project.body)
+    .then( () => {
+        return res_project.json({
+            erro: false,
+            mensagem: "Projeto cadastrado com sucesso!"
+        });
+    }).catch( () => {
+        return res_project.status(400).json({
+            erro: true,
+            mensagem: "Erro: Projeto não cadastrado com sucesso!"
+        });
+    });
+
+});
+
+
 
 app.listen(8080, () => {
     console.log("Servido iniciado na porta 8080: http://localhost:8080");
--- models/Project.js
+++ models/Project.js
@@ -1,24 +1,6 @@
-// criando as tabelas
 
 const Sequelize = require('sequelize');
 const db = require('./db');
-
-const User = db.define('Users', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-         allowNull: false,
-         primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    }
-});
 
 const Project = db.define('Projects', {
     id: {
@@ -35,11 +17,8 @@ const Project = db.define('Projects', {
         type: Sequelize.STRING,
         allowNull: false,
     },
-})
+});
 
 
-// criar a tabela
-//User.sync();
-module.exports = User;
 Project.sync();
 module.exports = Project;
