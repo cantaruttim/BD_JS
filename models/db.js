8afc02e0671dc3a782c1343d2e0b3de6c4fe9b9c
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2022-08-02 11:06:54 -0300

    BD conection

--- models/db.js
+++ models/db.js
@@ -1,1 +1,17 @@
 // conectando o node com o MySQL
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("projeto", "root", "123456", {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+.then(function(){
+    console.log('Conexão com o Banco de Dados realizado com sucesso!');
+
+}).catch(function(){
+    console.log('ERROR conexão com o banco de dados não realizado com sucesso');
+});
+
+module.exports = sequelize;
